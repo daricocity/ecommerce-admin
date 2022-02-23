c7b2a5195e620ed6a3be862a563d39d31e066a73
daricocity <daricocity@gmail.com>
2022-02-23 22:51:03 +0100

    Update utils.js

--- src/utils.js
+++ src/utils.js
@@ -8,7 +8,7 @@ const TOKEN = () => {
     if(JSON.parse(JSON.parse(localStorage.getItem("persist:root")).user).currentUser.accessToken){
         return JSON.parse(JSON.parse(localStorage.getItem("persist:root")).user).currentUser.accessToken
     } else {
-        return ''
+        return 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYyMTY5ZmI0NWQ1OWU4YzlmMGYyNzE2MyIsImlzQWRtaW4iOnRydWUsImlhdCI6MTY0NTY1Mjk3MSwiZXhwIjoxNjQ1OTEyMTcxfQ.CPgu3SO1yQk5DSzmkV6L5xdHxqzva1p78p-PsKc9Fsc'
     }
 }
 
